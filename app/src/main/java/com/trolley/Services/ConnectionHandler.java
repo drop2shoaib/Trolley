e510625d58093338abc3e29c46ecca61ee412f0f
Shoaib Khan <drop2shoaib@gmail.com>
2015-03-22 19:17:53 +0800

    Modified some files

--- app/src/main/java/com/trolley/Services/ConnectionHandler.java
+++ app/src/main/java/com/trolley/Services/ConnectionHandler.java
@@ -2,13 +2,14 @@ package com.trolley.Services;
 
 import android.util.Log;
 
+import com.trolley.utils.ApiKeys;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
