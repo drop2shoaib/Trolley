2e7a693518814ab1fa1b7842d3e6bf67049cd1cb
Shoaib Khan <drop2shoaib@gmail.com>
2015-03-28 21:40:52 +0800

    Added login and signup

--- app/src/main/java/com/trolley/Home.java
+++ app/src/main/java/com/trolley/Home.java
@@ -1,15 +1,55 @@
 package com.trolley;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.parse.ParseUser;
 
 /**
  * Created by sunny on 22/3/2015.
  */
-public class Home extends Activity {
+public class Home extends ActionBarActivity {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_logout:
+                logout();
+                return true;
+            case R.id.action_about:
+                showInfo();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void showInfo() {
+
+    }
+
+    private void logout() {
+        ParseUser.logOut();
+        startActivity(new Intent(this,LogIn.class)) ;
+        finish();
     }
 }
--- app/src/main/java/com/trolley/LogIn.java
+++ app/src/main/java/com/trolley/LogIn.java
@@ -1,18 +1,18 @@
 package com.trolley;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
-import com.trolley.custom.CustomFragment;
 import com.trolley.utils.CONSTS;
 
 /**
@@ -20,7 +20,9 @@ import com.trolley.utils.CONSTS;
  */
 public class LogIn extends Activity implements View.OnClickListener {
     private EditText emailAddress, password;
-
+    private Button signIn;
+    private TextView signUp;
+    private ProgressBar spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,23 +35,44 @@ public class LogIn extends Activity implements View.OnClickListener {
     private void initializeVariables(Activity view) {
         this.emailAddress = (EditText) view.findViewById(R.id.signin_email);
         this.password = (EditText) view.findViewById(R.id.signin_password);
+        this.signIn = (Button) view.findViewById(R.id.signin_signit);
+        this.signIn.setOnClickListener(this);
+        this.signUp = (TextView) view.findViewById(R.id.signin_signup);
+        this.signUp.setOnClickListener(this);
+        this.spinner= (ProgressBar) view.findViewById(R.id.login_Progress_bar);
+        stopSpinner();
     }
 
+    private void startSpinner(){
+       this.spinner.setVisibility(View.VISIBLE);
+    }
+    private void stopSpinner(){
+        this.spinner.setVisibility(View.GONE);
+    }
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.signin_signit:
+                startSpinner();
                 ParseUser.logInInBackground(this.emailAddress.getText().toString(), this.password.getText().toString(), new LogInCallback() {
                     @Override
                     public void done(ParseUser user, ParseException exception) {
                         if (user != null) {
-                            Toast.makeText(LogIn.this, CONSTS.LOGGED_IN_SUCCESSFULLY, Toast.LENGTH_LONG);
+//                            Toast.makeText(LogIn.this, CONSTS.LOGGED_IN_SUCCESSFULLY, Toast.LENGTH_LONG);
+                            startActivity(new Intent(LogIn.this, Home.class));
+                            finish();
                         } else {
-                            Toast.makeText(LogIn.this, CONSTS.LOGGING_FAILED, Toast.LENGTH_LONG);
+                            Toast.makeText(LogIn.this, CONSTS.LOGGING_FAILED, Toast.LENGTH_LONG).show();
                         }
+                        stopSpinner();
                     }
                 });
                 break;
+            case R.id.signin_signup:
+                startActivity(new Intent(LogIn.this, SignUp.class));
+                finish();
+                break;
         }
     }
+
 }
